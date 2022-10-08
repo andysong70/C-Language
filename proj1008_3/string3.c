9d3cdab5b3066277e2e5b67b2df4bbc3a1402a7c
andysong <fasgu@naver.com>
2022-10-08 11:49:13 +0900

    second commit

---
+++ proj1008_3/string3.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "A barking dog never bites";
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	printf("문자열 %s의 길이는 %d입니다. \n", str, i);
+	return 0;
+}
